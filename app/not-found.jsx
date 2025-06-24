69915281bb844547e1bb400c3c90dfca7ae2127b
Vishal <vishal.2302175@sritcbe.ac.in>
2025-06-24 13:52:16 +0530

    [Fix]: Arrav Name changes

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -4,9 +4,9 @@ import React from "react";
 import Image from "next/image";
 export const metadata = {
   title:
-    "Page Not Found || Resonance &mdash; One & Multi Page React Nextjs Creative Template",
+    "Page Not Found || Arrav Resonance &mdash; One & Multi Page React Nextjs Creative Template",
   description:
-    "Resonance &mdash; One & Multi Page React Nextjs Creative Template",
+    " Arrav Resonance &mdash; One & Multi Page React Nextjs Creative Template",
 };
 export default function MainAboutPage1() {
   return (
